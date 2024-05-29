24d78a417bbb009938c4dd10361d2ea7a0d18cd5
Hung Nguyen <nthung250802@gmail.com>
2024-05-29 16:50:25 +0000

    Added ForgotPassword.java

---
+++ ISP392_Group1/src/java/signin/ForgotPassword.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package signin;
+
+import dal.DAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.account;
+import register.EmailService;
+
+@WebServlet(name = "ForgotPassword", urlPatterns = {"/ForgotPassword"})
+public class ForgotPassword extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ForgotPassword</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ForgotPassword at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+
+        DAO d = new DAO();
+
+        boolean checkMail = d.checkUserEmail(email);
+        
+        if (checkMail) {
+            // send mail
+        EmailService sm = new EmailService();
+
+        String code = sm.getRandom();
+
+        account account = new account();
+        account.setEmail(email);
+        account.setCode(code);
+        HttpSession session = request.getSession();
+        Thread emailThread = new Thread(() -> {
+            boolean sendMail = sm.sendEmail(account);
+            if (sendMail) {
+                session.setAttribute("authcode", account);
+                session.setAttribute("email", email);
+            } else {
+                // Handle email sending failure
+                request.setAttribute("error", "Failed to send verification email. Please try again.");
+            }}  );
+            // start thread to send mail
+            emailThread.start();
+        // verify code
+        response.sendRedirect("emailverificationFP.jsp");
+
+        } else {
+            // Handle email not found in the database
+            request.setAttribute("error", "Email not exist. Please try again.");
+            request.getRequestDispatcher("forgotpassword.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
